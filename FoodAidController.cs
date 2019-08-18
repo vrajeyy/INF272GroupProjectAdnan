565fa416925aad25deb1a34f26f030d60ae34d62
MagicCandy061 <u18091581@tuks.co.za>
2019-08-18 12:45:29 +0200

    Devon commit-1
    Inital design:
    layout
    donate
    controller
    home
    listings

---
+++ FoodAidController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication7.Controllers
+{
+    public class FoodAidController : Controller
+    {
+        // GET: FoodAid
+        public ActionResult Home()
+        {
+            return View();
+        }
+        public ActionResult Donate()
+        {
+            return View();
+        }
+        public ActionResult Listings()
+        {
+            return View();
+        }
+    }
+}
